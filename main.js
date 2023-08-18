3cd99591a78db6345009e01926d67bf5215c5b28
Einschleicher <116537717+Einschleicher@users.noreply.github.com>
2023-08-18 06:46:45 +0200

    Ini website via module in init.js

--- main.js
+++ main.js
@@ -7,26 +7,90 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
 /***/ "./src/index.js":
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-/***/ (() => {
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("content = document.querySelector(\"#content\");\n\ncontent.innerHTML = \n\n`\n<div id=\"header\">\n<ul>\n    <li><button>HOME</button></li>\n    <li><button>MENU</button></li>\n    <li><button>CONTACT</button></li>\n</ul>\n</div>\n<div id=\"page\">\n<img src=\"coffee.jpg\" alt=\"Coffee beans lying on a world map\">\n<h1>Welcome to BeanJoy Café</h1>\n<p>\n    Indulge in a Symphony of Aromatic Bliss\n</p>\n<p>\n    Step into a haven where every gulp narrates a tale and every instant is cherished. BeanJoy Café warmly invites you to encounter the craftsmanship of coffee like never before. Tucked away in the heart of Sunnyville, our charming coffee corner is more than just a spot to snag your daily caffeine fix – it's an oasis for all java devotees.\n</p>\n<h2>Our Roast, Your Odyssey</h2>\n<p>\n    At BeanJoy Café, we take pride in metamorphosing each coffee bean into a work of artistry. Our expert baristas compose every mug with accuracy and zeal, ensuring that every slurp whisks you away on an odyssey of lush textures and captivating bouquets. From creamy macchiatos to bold Americanos, we proffer a medley of choices to cater to every palate.\n</p>\n<h2>A Cosy Hideaway</h2>\n<p>\n    Our snug coffee bar radiates a congenial ambiance, beckoning you to unwind, bond, and shape moments. Whether you're in search of a tranquil nook to delve into literature, engrossed in heartwarming dialogues with pals, or simply seeking respite from the commotion of the outer world, BeanJoy Café provides the quintessential backdrop.\n</p>\n<h2>Community and Kinship</h2>\n<p>\n    Beyond exquisite coffee, BeanJoy Café fosters community. We believe that coffee holds the power to unite. That's why we host periodic soirées, workshops, and tastings, cultivating an environment where coffee aficionados can converge to learn, exchange, and revel in their mutual ardor.\n</p>\n<h2>Sustainability at Heart</h2>\n<p>\n    Our dedication transcends the cup. We conscientiously source our beans, championing principled farming practices that guarantee a positive influence on both societies and the ecosystem. Join us as we tread towards an eco-friendly future, one mug at a time.\n</p>\n<h2>Pay Us a Visit</h2>\n<p>\n    Embark on your escapade to BeanJoy Café. We anticipate the pleasure of serving you not just coffee, but an encounter that lingers well after your final sip. Allow the redolence to escort you, let the savors kindle you, and let our coffee bar evolve into your sanctuary.\n</p>\n<p class=\"address\">\n    123 Java Lane, Sunnyville\n</p>\n<p class=\"hours\">\n    Open Mon-Sat: 7 AM - 6 PM\n</p>\n<p>\n    <em>Your Coffee Excursion Embarks Here</em>\n</p>\n`;\n\n\n//# sourceURL=webpack://odin-restaurant-page/./src/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _init__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./init */ \"./src/init.js\");\n\n\n(0,_init__WEBPACK_IMPORTED_MODULE_0__.initPage)();\n\n//# sourceURL=webpack://odin-restaurant-page/./src/index.js?");
+
+/***/ }),
+
+/***/ "./src/init.js":
+/*!*********************!*\
+  !*** ./src/init.js ***!
+  \*********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   initPage: () => (/* binding */ initPage)\n/* harmony export */ });\nfunction initPage() {\n\n    let content = document.querySelector(\"#content\");\n\n    content.innerHTML = \n\n    `\n    <div id=\"header\">\n    <ul>\n        <li><button>HOME</button></li>\n        <li><button>MENU</button></li>\n        <li><button>CONTACT</button></li>\n    </ul>\n    </div>\n    <div id=\"page\">\n    <img src=\"coffee.jpg\" alt=\"Coffee beans lying on a world map\">\n    <h1>Welcome to BeanJoy Café</h1>\n    <p>\n        Indulge in a Symphony of Aromatic Bliss\n    </p>\n    <p>\n        Step into a haven where every gulp narrates a tale and every instant is cherished. BeanJoy Café warmly invites you to encounter the craftsmanship of coffee like never before. Tucked away in the heart of Sunnyville, our charming coffee corner is more than just a spot to snag your daily caffeine fix – it's an oasis for all java devotees.\n    </p>\n    <h2>Our Roast, Your Odyssey</h2>\n    <p>\n        At BeanJoy Café, we take pride in metamorphosing each coffee bean into a work of artistry. Our expert baristas compose every mug with accuracy and zeal, ensuring that every slurp whisks you away on an odyssey of lush textures and captivating bouquets. From creamy macchiatos to bold Americanos, we proffer a medley of choices to cater to every palate.\n    </p>\n    <h2>A Cosy Hideaway</h2>\n    <p>\n        Our snug coffee bar radiates a congenial ambiance, beckoning you to unwind, bond, and shape moments. Whether you're in search of a tranquil nook to delve into literature, engrossed in heartwarming dialogues with pals, or simply seeking respite from the commotion of the outer world, BeanJoy Café provides the quintessential backdrop.\n    </p>\n    <h2>Community and Kinship</h2>\n    <p>\n        Beyond exquisite coffee, BeanJoy Café fosters community. We believe that coffee holds the power to unite. That's why we host periodic soirées, workshops, and tastings, cultivating an environment where coffee aficionados can converge to learn, exchange, and revel in their mutual ardor.\n    </p>\n    <h2>Sustainability at Heart</h2>\n    <p>\n        Our dedication transcends the cup. We conscientiously source our beans, championing principled farming practices that guarantee a positive influence on both societies and the ecosystem. Join us as we tread towards an eco-friendly future, one mug at a time.\n    </p>\n    <h2>Pay Us a Visit</h2>\n    <p>\n        Embark on your escapade to BeanJoy Café. We anticipate the pleasure of serving you not just coffee, but an encounter that lingers well after your final sip. Allow the redolence to escort you, let the savors kindle you, and let our coffee bar evolve into your sanctuary.\n    </p>\n    <p class=\"address\">\n        123 Java Lane, Sunnyville\n    </p>\n    <p class=\"hours\">\n        Open Mon-Sat: 7 AM - 6 PM\n    </p>\n    <p>\n        <em>Your Coffee Excursion Embarks Here</em>\n    </p>\n    `;\n\n    console.log(\"initialization done\");\n};\n\n\n\n//# sourceURL=webpack://odin-restaurant-page/./src/init.js?");
 
 /***/ })
 
 /******/ 	});
 /************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
 /******/ 	
 /******/ })()
 ;
