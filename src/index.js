38e9dcddbd26afd77bfb5344f58a6e4c4b3e75c2
Einschleicher <116537717+Einschleicher@users.noreply.github.com>
2023-08-19 20:41:06 +0200

    Add event listeners for the buttons

--- src/index.js
+++ src/index.js
@@ -3,5 +3,23 @@ import { loadMenu } from "./menu";
 import { loadContacts } from "./contact";
 
 initPage();
-loadMenu();
-loadContacts();
+setEventListeners();
+
+function setEventListeners() {
+    let homeButton = document.querySelector("li:nth-child(1) button");
+    let menuButton = document.querySelector("li:nth-child(2) button");
+    let contactsButton = document.querySelector("li:nth-child(3) button");
+    
+    homeButton.addEventListener("click", function() {
+        initPage();
+        setEventListeners();        
+    });
+    menuButton.addEventListener("click", function() {
+        loadMenu();
+        setEventListeners();
+    });
+    contactsButton.addEventListener("click", function() {
+        loadContacts();
+        setEventListeners();
+    });
+}
